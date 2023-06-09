db46a9bec068f24f643c1c4c20e29ebcdd386be5
VarvaraKSemenova <111306119+VarvaraKSemenova@users.noreply.github.com>
2023-06-09 17:26:49 +0300

    Update fr_generation.py

--- fr_generation.py
+++ fr_generation.py
@@ -3,18 +3,16 @@ from pyspark.mllib.random import RandomRDDs
 from pyspark.sql.functions import col, from_unixtime, floor, udf, lit, \
     to_timestamp, when, expr, row_number, monotonically_increasing_id, array, explode
 from pyspark.sql.types import StructType, TimestampType, IntegerType, StructField, DoubleType
-# Left join
-# TX_FRAUD, TX_FRAUD_SCENARIO
+
+"""
+Generate and mark fraude transactions 
+"""
+
 spark = SparkSession.builder.appName("generate-transactions").getOrCreate()
-# spark.sparkContext.setLogLevel('WARN')
 customer_list = spark.read.parquet('/user/dataproc-agent/customer_list')
 terminals_list = spark.read.parquet('/user/dataproc-agent/terminals_list')
 transaction_list = spark.read.csv('/user/dataproc-agent/transactions_list')
-#
-# 'TX_DATETIME', 'CUSTOMER_ID',
-#                                                  'TERMINAL_ID',
-#                                                  'TX_AMOUNT', 'TX_TIME_SECONDS',
-#                                                  'TX_TIME_DAYS'
+
 transaction_list = transaction_list.withColumnRenamed('_c0', 'TX_DATETIME')
 transaction_list = transaction_list.withColumnRenamed('_c1', 'CUSTOMER_ID')
 transaction_list = transaction_list.withColumnRenamed('_c2', 'TERMINAL_ID')
@@ -44,35 +42,12 @@ def get_customer(x):
 
 udf_get_customer = udf(get_customer, IntegerType())
 
-print('\nGet max')
-n_days = 2000 * 91 # transaction_list.select('TX_TIME_DAYS').rdd.max()[0]
-transaction_list = transaction_list.filter(col('TX_TIME_DAYS') <= 2000*20*4).filter(col('TX_TIME_DAYS') > 2000*20*3)
-print('Max = ', n_days)
+
+n_days = transaction_list.select('TX_TIME_DAYS').rdd.max()[0]
 n_compr_terminals = 2
 n_compr_customers = 2
 
-# print('\nGenerate ter')
-# compromised_terminals_list = RandomRDDs.uniformVectorRDD(
-#     spark.sparkContext, n_days, n_compr_terminals
-# ).map(
-#         lambda a: a.tolist()
-# ).toDF()
-# print('\nTransform ter')
-# compromised_terminals_list = compromised_terminals_list.withColumn(
-#     '_1', floor(col('_1') / weight_terminals)
-# ).withColumn(
-#     '_2', floor(col('_2') / weight_terminals)
-# ).withColumnRenamed('_1', 'ter_1').withColumnRenamed('_2', 'ter_2').withColumn(
-#     'ter_2', when(col('ter_1') == col('ter_2'), col('ter_2')+lit(1)).otherwise(col('ter_2'))
-# ).withColumn(
-#     'ter_1', udf_get_terminal(col('ter_1'))
-# ).withColumn(
-#     'ter_2', udf_get_terminal(col('ter_2'))
-# ).withColumn(
-#     'id_n', monotonically_increasing_id()
-# ).withColumn('day', row_number().over(window=Window.orderBy('id_n'))).drop('id_n')
-#
-# compromised_terminals_list.write.csv('compromised_terminals', mode='overwrite', header='True')
+# Get list of compromised terminals
 compromised_terminals_list = spark.read.csv('/user/dataproc-agent/compromised_terminals',  header='True')
 
 compromised_terminals_list = compromised_terminals_list.withColumn(
@@ -85,31 +60,7 @@ compromised_terminals_list = compromised_terminals_list.withColumn(
     'ndays', explode(col('ndays'))
 ).withColumn('day', col('day') + col('ndays')).drop('ndays')
 
-# print('\nGenerate cus')
-# compromised_customers_list = RandomRDDs.uniformVectorRDD(
-#     spark.sparkContext, n_days, n_compr_customers
-# ).map(
-#         lambda a: a.tolist()
-# ).toDF()
-#
-#
-# print('\nTransform cus')
-# compromised_customers_list = compromised_customers_list.withColumn(
-#     '_1', floor(col('_1') / weight_customer)
-# ).withColumn(
-#     '_2', floor(col('_2') / weight_customer)
-# ).withColumnRenamed('_1', 'cust_1').withColumnRenamed('_2', 'cust_2').withColumn(
-#     'cust_2', when(col('cust_1') == col('cust_2'), col('cust_2')+lit(1)).otherwise(col('cust_2'))
-# ).withColumn(
-#     'cust_1', udf_get_terminal(col('cust_1'))
-# ).withColumn(
-#     'cust_2', udf_get_terminal(col('cust_2'))
-# ).withColumn(
-#     'id_n', monotonically_increasing_id()
-# ).withColumn('day', row_number().over(window=Window.orderBy('id_n'))).drop('id_n')
-#
-# compromised_customers_list.write.csv('compromised_customers', mode='overwrite', header='True')
-
+# Get list of compromised customers
 compromised_customers_list = spark.read.csv('/user/dataproc-agent/compromised_customers',  header='True')
 
 compromised_customers_list = compromised_customers_list.withColumn(
@@ -122,35 +73,39 @@ compromised_customers_list = compromised_customers_list.withColumn(
     'ndays', explode(col('ndays'))
 ).withColumn('day', col('day') + col('ndays')).drop('ndays')
 
-print('\nFirst joint started!')
+# Mark compromised terminals
 transaction_list = transaction_list.join(
     compromised_terminals_list, (
         (transaction_list['TERMINAL_ID'] == compromised_terminals_list['term']) &
         (transaction_list['TX_TIME_DAYS'] == compromised_terminals_list['day'])
     ), 'left'
 ).drop('day', 'term')
-print('\nSecond join started!')
+
+# Mark compromised customers
 transaction_list = transaction_list.join(
     compromised_customers_list, (
         (transaction_list['CUSTOMER_ID'] == compromised_customers_list['cust']) &
         (transaction_list['TX_TIME_DAYS'] == compromised_customers_list['day'])
     ), 'left'
 ).drop('day', 'cust')
-print('\nSecond join finished')
+
 
 transaction_list = transaction_list.fillna(0)
 
-print('\nset TX_FRAUD_SCENARIO')
+
+# Set fraud scenarios
 transaction_list = transaction_list.withColumn(
     'TX_FRAUD_SCENARIO',
     when(col('compr_cust') > 0, lit(3)).when(
         col('compr_term') > 0, lit(2)).when(col('TX_AMOUNT') > 220, lit(1)).otherwise(lit(0))
 )
-print('\nset TX_FRAUD')
+
+# Mark frauds
 transaction_list = transaction_list.withColumn(
     'TX_FRAUD', when(col('TX_FRAUD_SCENARIO') > 0, lit(1)).otherwise(lit(0))
 )
-print('\nset TX_AMOUNT')
+
+# Geterate situations when customer is compromised
 transaction_list = transaction_list.withColumn(
     'TX_AMOUNT',
     when(col('TX_FRAUD_SCENARIO') == 3, col('TX_AMOUNT') * lit(5)).otherwise(col('TX_AMOUNT'))
@@ -158,7 +113,5 @@ transaction_list = transaction_list.withColumn(
 
 compromised_terminals_list.unpersist()
 compromised_customers_list.unpersist()
-print('start write')
-# transaction_list.show()
 
 transaction_list.write.csv('transactions_list_with_marks', mode='append', header='True')
